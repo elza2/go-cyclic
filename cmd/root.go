5d53a77dd41137621633bb3329368bcfbe147df4
yuanyou <yuanyou@kezaihui.com>
2023-01-12 20:20:29 +0800

    feat: optimized execution instruction.

--- cmd/root.go
+++ cmd/root.go
@@ -7,20 +7,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const Service = "github.com/elza2/go-cyclic"
-
-var (
-	rootCmd = &cobra.Command{Use: Service}
-)
-
 func Execute() {
+	rootCmd := &cobra.Command{
+		Run: RunCyclic,
+	}
+
+	rootCmd.PersistentFlags().String("dir", os.Getenv("dir"), "dir. eg: directory address of the go.mod file")
+	rootCmd.PersistentFlags().String("filter", os.Getenv("filter"), "dir. eg: filters out files of the specified type. separate multiple types with commas (,)")
+
 	if err := rootCmd.Execute(); err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
-}
-
-func init() {
-	rootCmd.PersistentFlags().String("dir", os.Getenv("dir"), "dir. eg: directory address of the go.mod file")
-	rootCmd.PersistentFlags().String("filter", os.Getenv("filter"), "dir. eg: filters out files of the specified type. separate multiple types with commas (,)")
 }
